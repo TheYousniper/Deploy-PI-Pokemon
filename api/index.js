99902e78a69013b5f2e534ac7621519f8bc37c74
johalama2023 <johalama2023@gmail.com>
2023-06-08 09:53:21 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -20,11 +20,13 @@
 const server = require('./src/app.js');
 const initialTypes = require('./src/controllers/initialTypes.js');
 const { conn } = require('./src/db.js');
+require("dotenv").config();
+const {PORT} = process.env;
 
 // Syncing all the models at once.force
 conn.sync({ force : true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at', PORT); // eslint-disable-line no-console
     initialTypes()
   });
 });
